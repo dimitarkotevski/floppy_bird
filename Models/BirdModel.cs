588cd41ed4dccbe3623bbe6ff787ff0fdebdfc81
Котевски Димитар <dimitar.kotevski@students.finki.ukim.mk>
2022-07-05 11:25:12 +0200

    Falling box

---
+++ Models/BirdModel.cs
@@ -0,0 +1,11 @@
+﻿namespace floppy_bird_aliexpres.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromGround { get; set; } = 100;
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= gravity;
+        }
+    }
+}
---
+++ Models/GameManager.cs
@@ -0,0 +1,30 @@
+﻿
+
+using System.ComponentModel;
+
+namespace floppy_bird_aliexpres.Models
+{
+    public class GameManager : INotifyPropertyChanged
+    {
+        private readonly int gravity = 2;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public BirdModel Bird { get; set; }
+        public bool isRunning { get; set; } = false;
+        public GameManager()
+        {
+            Bird=new BirdModel();
+        }
+        public async void MainLoop()
+        {
+            isRunning = true;
+            while (isRunning)
+            {
+                Bird.Fall(gravity);
+                PropertyChanged?.Invoke(this,new PropertyChangedEventArgs(nameof(Bird)));
+                await Task.Delay(20);
+            }
+        }
+    }
+}
